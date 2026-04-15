6fc8065be9e3f00bcae5541ee13d9a4e6a8956f3
Olivio <edvaldoolivio3@gmail.com>
2026-04-15 17:08:41 -0300

    erro consertado

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import { Map, GraduationCap, ChevronDown } from 'lucide-react';
+import { GraduationCap, ChevronDown } from 'lucide-react';
 
 export default function Hero() {
   return (
